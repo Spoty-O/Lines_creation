28f4edcbb262f241a8c68e0eb3cf0c07fa100ef0
dkorotysh <dkorotysh@stud.khpi.ucode-connect.study>
2022-10-21 18:45:42 +0300

    work

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 var Field = /** @class */ (function () {
     function Field() {
+        var _this = this;
         var _a;
         this.canvas = document.getElementById('canvas');
         this.ctx = (_a = this.canvas) === null || _a === void 0 ? void 0 : _a.getContext('2d');
-        this.temp_line = null;
+        this.temp_line = undefined;
+        this.collapse_line = undefined;
         this.lines_arr = [];
+        this.render_all = function () {
+            var _a;
+            (_a = _this.ctx) === null || _a === void 0 ? void 0 : _a.clearRect(0, 0, _this.canvas.width, _this.canvas.height);
+            _this.render_lines();
+            _this.render_intersect_points();
+        };
+        this.canvas.oncontextmenu = function () { return false; };
         console.log(this.canvas);
     }
     Field.prototype.render_point = function (point) {
@@ -30,11 +24,10 @@ var Field = /** @class */ (function () {
         (_c = this.ctx) === null || _c === void 0 ? void 0 : _c.fill();
         (_d = this.ctx) === null || _d === void 0 ? void 0 : _d.stroke();
     };
-    Field.prototype.render_intersect_points = function (point_coor) {
-        if (point_coor === void 0) { point_coor = undefined; }
+    Field.prototype.render_intersect_points = function () {
         for (var _i = 0, _a = this.lines_arr; _i < _a.length; _i++) {
             var line = _a[_i];
-            for (var _b = 0, _c = line.intersect_point(point_coor); _b < _c.length; _b++) {
+            for (var _b = 0, _c = line.intersect_point(); _b < _c.length; _b++) {
                 var point = _c[_b];
                 this.render_point(point);
             }
@@ -42,102 +35,108 @@ var Field = /** @class */ (function () {
     };
     Field.prototype.set_point = function (point) {
         if (this.temp_line) {
-            this.temp_line.set_end(point.x, point.y);
-            this.lines_arr.push(this.temp_line);
-            this.temp_line = null;
+            this.temp_line.set_end(point);
+            this.temp_line = undefined;
         }
         else {
             this.temp_line = new Line();
-            this.temp_line.set_start(point.x, point.y);
+            this.lines_arr.push(this.temp_line);
+            this.temp_line.set_start(point);
         }
-        this.render_all();
+    };
+    Field.prototype.unset_line = function () {
+        if (this.lines_arr.length != 0 && this.temp_line) {
+            this.lines_arr.pop();
+            this.temp_line = undefined;
+            requestAnimationFrame(this.render_all);
+        }
     };
     Field.prototype.render_temp_line = function (point) {
         if (this.temp_line) {
-            this.render_all();
-            this.render_line(this.temp_line.start_coordinates, point);
-            this.render_intersect_points(point);
+            this.temp_line.set_end(point);
+            requestAnimationFrame(this.render_all);
         }
     };
-    Field.prototype.render_line = function (start_point, end_point) {
+    Field.prototype.render_lines = function () {
         var _a, _b, _c, _d;
-        (_a = this.ctx) === null || _a === void 0 ? void 0 : _a.beginPath();
-        (_b = this.ctx) === null || _b === void 0 ? void 0 : _b.moveTo(start_point.x, start_point.y);
-        (_c = this.ctx) === null || _c === void 0 ? void 0 : _c.lineTo(end_point.x, end_point.y);
-        (_d = this.ctx) === null || _d === void 0 ? void 0 : _d.stroke();
-    };
-    Field.prototype.render_all = function () {
-        var _a;
-        (_a = this.ctx) === null || _a === void 0 ? void 0 : _a.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        for (var _i = 0, _b = this.lines_arr; _i < _b.length; _i++) {
-            var line = _b[_i];
-            this.render_line(line.start_coordinates, line.end_coordinates);
+        for (var _i = 0, _e = this.lines_arr; _i < _e.length; _i++) {
+            var _f = _e[_i], start_coordinates = _f.start_coordinates, end_coordinates = _f.end_coordinates;
+            (_a = this.ctx) === null || _a === void 0 ? void 0 : _a.beginPath();
+            (_b = this.ctx) === null || _b === void 0 ? void 0 : _b.moveTo(start_coordinates.x, start_coordinates.y);
+            (_c = this.ctx) === null || _c === void 0 ? void 0 : _c.lineTo(end_coordinates.x, end_coordinates.y);
+            (_d = this.ctx) === null || _d === void 0 ? void 0 : _d.stroke();
         }
-        this.render_intersect_points();
+    };
+    Field.prototype.collapse = function () {
+        var _this = this;
+        var x = (this.collapse_line.start_coordinates.x + this.collapse_line.end_coordinates.x) / 2;
+        var y = (this.collapse_line.start_coordinates.y + this.collapse_line.end_coordinates.y) / 2;
+        var x1, x2, y1, y2;
+        console.log("wokr");
+        setInterval(function () {
+            var _a, _b;
+            // if (this.collapse_line!.start_coordinates.x == x + 10)
+            //     return
+            x1 = _this.collapse_line.start_coordinates.x < x ? _this.collapse_line.start_coordinates.x + 1 : _this.collapse_line.start_coordinates.x - 1;
+            x2 = _this.collapse_line.end_coordinates.x < x ? _this.collapse_line.end_coordinates.x + 1 : _this.collapse_line.end_coordinates.x - 1;
+            y1 = _this.collapse_line.start_coordinates.y < y ? _this.collapse_line.start_coordinates.y + 1 : _this.collapse_line.start_coordinates.y - 1;
+            y2 = _this.collapse_line.end_coordinates.y < y ? _this.collapse_line.end_coordinates.y + 1 : _this.collapse_line.end_coordinates.y - 1;
+            console.log(x1, x2, x);
+            (_a = _this.collapse_line) === null || _a === void 0 ? void 0 : _a.set_start({ x: x1, y: y1 });
+            (_b = _this.collapse_line) === null || _b === void 0 ? void 0 : _b.set_end({ x: x2, y: y2 });
+            requestAnimationFrame(_this.render_all);
+        }, 100);
+        // this.lines_arr.shift();
+    };
+    Field.prototype.collapse_lines = function () {
+        for (var _i = 0, _a = this.lines_arr; _i < _a.length; _i++) {
+            var line = _a[_i];
+            this.collapse_line = line;
+            this.collapse();
+        }
     };
     return Field;
 }());
-var Line = /** @class */ (function (_super) {
-    __extends(Line, _super);
+var Line = /** @class */ (function () {
     function Line() {
-        return _super !== null && _super.apply(this, arguments) || this;
     }
-    Line.prototype.set_start = function (x, y) {
-        this.start_coordinates = new Point(x, y);
+    Line.prototype.set_start = function (point) {
+        this.start_coordinates = point;
     };
-    Line.prototype.set_end = function (x, y) {
-        this.end_coordinates = new Point(x, y);
+    Line.prototype.set_end = function (point) {
+        this.end_coordinates = point;
     };
-    Line.prototype.intersec_check = function (a, b, c, d) {
-        var common = (b.x - a.x) * (d.y - c.y) - (b.y - a.y) * (d.x - c.x);
-        if (common == 0)
-            return false;
-        var rH = (a.y - c.y) * (d.x - c.x) - (a.x - c.x) * (d.y - c.y);
-        var sH = (a.y - c.y) * (b.x - a.x) - (a.x - c.x) * (b.y - a.y);
-        var r = rH / common;
-        var s = sH / common;
-        if (r >= 0 && r <= 1 && s >= 0 && s <= 1)
-            return true;
-        else
-            return false;
+    Line.prototype.lerp = function (A, B, t) {
+        return A + (B - A) * t;
     };
-    Line.prototype.intersect_point = function (end_point) {
-        if (end_point === void 0) { end_point = this.end_coordinates; }
+    Line.prototype.intersect_point = function () {
         var points = [];
-        var a1, b1, c1, a2, b2, c2, det, x, y;
-        if (end_point) {
-            for (var _i = 0, _a = field.lines_arr; _i < _a.length; _i++) {
-                var line = _a[_i];
-                if (this.intersec_check(this.start_coordinates, end_point, line.start_coordinates, line.end_coordinates) == true) {
-                    console.log(this.start_coordinates, end_point, line.start_coordinates, line.end_coordinates);
-                    a1 = this.start_coordinates.y - end_point.y;
-                    b1 = this.end_coordinates.x - this.start_coordinates.x;
-                    c1 = this.start_coordinates.x * end_point.y - end_point.x * this.start_coordinates.y;
-                    a2 = line.start_coordinates.y - line.end_coordinates.y;
-                    b2 = line.end_coordinates.x - line.start_coordinates.x;
-                    c2 = line.start_coordinates.x * line.end_coordinates.y - line.end_coordinates.x * line.start_coordinates.y;
-                    det = a1 * b2 - a2 * b1;
-                    x = (b1 * c2 - b2 * c1) / det;
-                    y = (a2 * c1 - a1 * c2) / det;
-                    points.push(new Point(x, y));
+        var tTop, uTop, bottom, t, u;
+        for (var _i = 0, _a = field.lines_arr; _i < _a.length; _i++) {
+            var line = _a[_i];
+            tTop = (line.end_coordinates.x - line.start_coordinates.x) * (this.start_coordinates.y - line.start_coordinates.y) - (line.end_coordinates.y - line.start_coordinates.y) * (this.start_coordinates.x - line.start_coordinates.x);
+            uTop = (line.start_coordinates.y - this.start_coordinates.y) * (this.start_coordinates.x - this.end_coordinates.x) - (line.start_coordinates.x - this.start_coordinates.x) * (this.start_coordinates.y - this.end_coordinates.y);
+            bottom = (line.end_coordinates.y - line.start_coordinates.y) * (this.end_coordinates.x - this.start_coordinates.x) - (line.end_coordinates.x - line.start_coordinates.x) * (this.end_coordinates.y - this.start_coordinates.y);
+            if (bottom != 0) {
+                t = tTop / bottom;
+                u = uTop / bottom;
+                if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
+                    points.push({ x: this.lerp(this.start_coordinates.x, this.end_coordinates.x, t), y: this.lerp(this.start_coordinates.y, this.end_coordinates.y, t) });
                 }
             }
         }
         return points;
     };
     return Line;
-}(Field));
-var Point = /** @class */ (function () {
-    function Point(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-    return Point;
 }());
 var field = new Field();
 field.canvas.addEventListener("click", function (event) {
-    field.set_point(new Point(event.offsetX, event.offsetY));
+    field.set_point({ x: event.offsetX, y: event.offsetY });
+}, false);
+field.canvas.addEventListener("contextmenu", function (event) {
+    field.unset_line();
 }, false);
 field.canvas.addEventListener("mousemove", function (event) {
-    field.render_temp_line(new Point(event.offsetX, event.offsetY));
+    field.render_temp_line({ x: event.offsetX, y: event.offsetY });
 }, false);
+document.getElementById('button').onclick = function () { console.log("Now not realised"); };
--- script.ts
+++ script.ts
@@ -1,11 +1,13 @@
 class Field {
     canvas = <HTMLCanvasElement>document.getElementById('canvas');
     ctx: CanvasRenderingContext2D | null = this.canvas?.getContext('2d');
-    temp_line: Line | null = null;
+    temp_line: Line | undefined = undefined;
+    collapse_line: Line | undefined = undefined;
     lines_arr: Line[] = [];
 
     constructor() {
-        console.log(this.canvas)
+        this.canvas.oncontextmenu = () => { return false; };
+        console.log(this.canvas);
     }
 
     render_point(point: Point): void {
@@ -16,9 +18,9 @@ class Field {
         this.ctx?.stroke();
     }
 
-    render_intersect_points(point_coor: Point | undefined = undefined): void {
+    render_intersect_points(): void {
         for (let line of this.lines_arr) {
-            for (let point of line.intersect_point(point_coor)) {
+            for (let point of line.intersect_point()) {
                 this.render_point(point);
             }
         }
@@ -26,110 +28,125 @@ class Field {
 
     set_point(point: Point): void {
         if (this.temp_line) {
-            this.temp_line.set_end(point.x, point.y);
-            this.lines_arr.push(this.temp_line);
-            this.temp_line = null;
+            this.temp_line.set_end(point);
+            this.temp_line = undefined;
         } else {
             this.temp_line = new Line();
-            this.temp_line.set_start(point.x, point.y);
+            this.lines_arr.push(this.temp_line);
+            this.temp_line.set_start(point);
         }
-        this.render_all();
+    }
+
+    unset_line(): void {
+        if (this.lines_arr.length != 0 && this.temp_line) {
+            this.lines_arr.pop();
+            this.temp_line = undefined;
+            requestAnimationFrame(this.render_all);
+        }
     }
 
     render_temp_line(point: Point): void {
         if (this.temp_line) {
-            this.render_all();
-            this.render_line(this.temp_line.start_coordinates, point);
-            this.render_intersect_points(point);
+            this.temp_line.set_end(point);
+            requestAnimationFrame(this.render_all);
         }
     }
 
-    render_line(start_point: Point, end_point: Point): void {
-        this.ctx?.beginPath();
-        this.ctx?.moveTo(start_point.x, start_point.y);
-        this.ctx?.lineTo(end_point.x, end_point.y);
-        this.ctx?.stroke();
+    render_lines(): void {
+        for (let { start_coordinates, end_coordinates } of this.lines_arr) {
+            this.ctx?.beginPath();
+            this.ctx?.moveTo(start_coordinates.x, start_coordinates.y);
+            this.ctx?.lineTo(end_coordinates.x, end_coordinates.y);
+            this.ctx?.stroke();
+        }
     }
 
-    render_all(): void {
+    render_all = (): void => {
         this.ctx?.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        for (let line of this.lines_arr) {
-            this.render_line(line.start_coordinates, line.end_coordinates);
-        }
+        this.render_lines();
         this.render_intersect_points();
+    }
+
+    collapse(): void {
+        let x: number = (this.collapse_line!.start_coordinates.x + this.collapse_line!.end_coordinates.x) / 2;
+        let y: number = (this.collapse_line!.start_coordinates.y + this.collapse_line!.end_coordinates.y) / 2;
+        let x1: number, x2: number, y1: number, y2: number;
+        console.log("wokr")
+        setInterval(() => {
+            // if (this.collapse_line!.start_coordinates.x == x + 10)
+            //     return
+            x1 = this.collapse_line!.start_coordinates.x < x ? this.collapse_line!.start_coordinates.x + 1 : this.collapse_line!.start_coordinates.x - 1;
+            x2 = this.collapse_line!.end_coordinates.x < x ? this.collapse_line!.end_coordinates.x + 1 : this.collapse_line!.end_coordinates.x - 1;
+            y1 = this.collapse_line!.start_coordinates.y < y ? this.collapse_line!.start_coordinates.y + 1 : this.collapse_line!.start_coordinates.y - 1;
+            y2 = this.collapse_line!.end_coordinates.y < y ? this.collapse_line!.end_coordinates.y + 1 : this.collapse_line!.end_coordinates.y - 1;
+            console.log(x1, x2, x)
+            this.collapse_line?.set_start({ x: x1, y: y1 });
+            this.collapse_line?.set_end({ x: x2, y: y2 });
+            requestAnimationFrame(this.render_all);
+        }, 100)
+        // this.lines_arr.shift();
+    }
+
+    collapse_lines(): void {
+        for (let line of this.lines_arr) {
+            this.collapse_line = line;
+            this.collapse();
+        }
     }
 }
 
-class Line extends Field {
+class Line {
     start_coordinates: Point;
     end_coordinates: Point;
 
-    set_start(x: number, y: number): void {
-        this.start_coordinates = new Point(x, y);
+    set_start(point: Point): void {
+        this.start_coordinates = point;
     }
 
-    set_end(x: number, y: number): void {
-        this.end_coordinates = new Point(x, y);
+    set_end(point: Point): void {
+        this.end_coordinates = point;
     }
 
-    intersec_check(a: Point, b: Point, c: Point, d: Point): boolean {
-        let common: number = (b.x - a.x) * (d.y - c.y) - (b.y - a.y) * (d.x - c.x);
-
-        if (common == 0) return false;
-
-        let rH: number = (a.y - c.y) * (d.x - c.x) - (a.x - c.x) * (d.y - c.y);
-        let sH: number = (a.y - c.y) * (b.x - a.x) - (a.x - c.x) * (b.y - a.y);
-
-        let r: number = rH / common;
-        let s: number = sH / common;
-
-        if (r >= 0 && r <= 1 && s >= 0 && s <= 1)
-            return true;
-        else
-            return false;
+    lerp(A: number, B: number, t: number) {
+        return A + (B - A) * t;
     }
 
-    intersect_point(end_point: Point = this.end_coordinates): Point[] {
+    intersect_point(): Point[] {
         let points: Point[] = [];
-        let a1: number, b1: number, c1: number, a2: number, b2: number, c2: number, det: number, x: number, y: number;
-        if (end_point) {
-            for (let line of field.lines_arr) {
-                if (this.intersec_check(this.start_coordinates, end_point, line.start_coordinates, line.end_coordinates)) {
-                    console.log(this.start_coordinates, end_point, line.start_coordinates, line.end_coordinates)
-                    a1 = this.start_coordinates.y - end_point.y;
-                    b1 = this.end_coordinates.x - this.start_coordinates.x;
-                    c1 = this.start_coordinates.x * end_point.y - end_point.x * this.start_coordinates.y;
-                    a2 = line.start_coordinates.y - line.end_coordinates.y;
-                    b2 = line.end_coordinates.x - line.start_coordinates.x;
-                    c2 = line.start_coordinates.x * line.end_coordinates.y - line.end_coordinates.x * line.start_coordinates.y;
-                    det = a1 * b2 - a2 * b1;
-                    x = (b1 * c2 - b2 * c1) / det;
-                    y = (a2 * c1 - a1 * c2) / det;
-                    points.push(new Point(x, y));
+        let tTop: number, uTop: number, bottom: number, t: number, u: number;
+        for (let line of field.lines_arr) {
+            tTop = (line.end_coordinates.x - line.start_coordinates.x) * (this.start_coordinates.y - line.start_coordinates.y) - (line.end_coordinates.y - line.start_coordinates.y) * (this.start_coordinates.x - line.start_coordinates.x);
+            uTop = (line.start_coordinates.y - this.start_coordinates.y) * (this.start_coordinates.x - this.end_coordinates.x) - (line.start_coordinates.x - this.start_coordinates.x) * (this.start_coordinates.y - this.end_coordinates.y);
+            bottom = (line.end_coordinates.y - line.start_coordinates.y) * (this.end_coordinates.x - this.start_coordinates.x) - (line.end_coordinates.x - line.start_coordinates.x) * (this.end_coordinates.y - this.start_coordinates.y);
+            if (bottom != 0) {
+                t = tTop / bottom;
+                u = uTop / bottom;
+                if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
+                    points.push({ x: this.lerp(this.start_coordinates.x, this.end_coordinates.x, t), y: this.lerp(this.start_coordinates.y, this.end_coordinates.y, t) });
                 }
             }
         }
         return points;
     }
-
 }
 
-class Point {
+interface Point {
     x: number;
     y: number;
-
-    constructor(x: number, y: number) {
-        this.x = x;
-        this.y = y;
-    }
 }
 
 let field = new Field();
 
 field.canvas.addEventListener("click", (event) => {
-    field.set_point(new Point(event.offsetX, event.offsetY));
+    field.set_point({ x: event.offsetX, y: event.offsetY });
+}, false);
+
+field.canvas.addEventListener("contextmenu", (event) => {
+    field.unset_line();
 }, false);
 
 field.canvas.addEventListener("mousemove", (event) => {
-    field.render_temp_line(new Point(event.offsetX, event.offsetY));
+    field.render_temp_line({ x: event.offsetX, y: event.offsetY });
 }, false);
+
+document.getElementById('button')!.onclick = () => { console.log("Now not realised") };
